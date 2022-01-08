8a688b4f9b439e2761bc25e4617406a9a0c6b1ed
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-01-09 02:38:06 +0530

    define main activity objects and connect with xml

--- MyTODO/app/src/main/java/com/user/mytodo/MainActivity.java
+++ MyTODO/app/src/main/java/com/user/mytodo/MainActivity.java
@@ -2,13 +2,38 @@ package com.user.mytodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //initialize objects
+    private Button btnAdd;
+    private ListView listView;
+    private TextView count;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //connect with xml using ids
+        btnAdd = findViewById(R.id.btnAdd);
+        listView = findViewById(R.id.todoList);
+        count = findViewById(R.id.count);
+        context = this;
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {  //onClickLister
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(context, AddTodo.class));//create intent class to redirect to AddTodo activity
+            }
+        });
     }
 }
