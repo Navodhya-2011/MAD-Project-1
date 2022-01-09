03b5f5b0178e4e96b2b737eb2f584755371bcf88
Sasindu Nanayakkara <73772797+SasinduNanayakkara@users.noreply.github.com>
2022-01-09 14:52:19 +0530

    add submit button functionalities

--- MyTODO/app/src/main/java/com/user/mytodo/AddTodo.java
+++ MyTODO/app/src/main/java/com/user/mytodo/AddTodo.java
@@ -2,6 +2,8 @@ package com.user.mytodo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,7 +12,7 @@ public class AddTodo extends AppCompatActivity {
 
     //initialize objects
     private EditText title,desc;
-    private Button add;
+    private DBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,20 @@ public class AddTodo extends AppCompatActivity {
         //connect with xml using ids
         title = findViewById(R.id.titleInput);
         desc = findViewById(R.id.descInput);
-        add = findViewById(R.id.btnSubmit);
+        Button btnSubmit = findViewById(R.id.btnSubmit);
+        Context context = this;
+
+        dbHandler = new DBHandler(context); //create database handler object
+
+        btnSubmit.setOnClickListener(view -> {
+            String userTitle = title.getText().toString();//get and assign user input title
+            String userDesc = desc.getText().toString();//get and assign user input description
+            long started = System.currentTimeMillis();//get and assign current date and time from the system
+
+            Todo todo = new Todo(userTitle,userDesc,started,0);//pass the data to the model class
+            dbHandler.addTodo(todo); //pass the data to the database handler
+
+            startActivity(new Intent(context, MainActivity.class)); //redirect to the main activity
+        });
     }
 }
